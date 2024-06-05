71136f50c7c1caefd175f848f06e7aea7861b4c8
NaoY220 <129801469+NaoY220@users.noreply.github.com>
2024-06-04 21:14:02 -0700

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -7,17 +7,17 @@ public class MyArrayList {
         list = new ArrayList<>();
     }
 
-    // Method to add an element to the ArrayList
+    //fuction to add element
     public void add(int data) {
         list.add(data);
     }
 
-    // Method to get an element from the ArrayList
+    // function to get the element at desired index
     public int get(int index) {
         return list.get(index);
     }
 
-    // Method to print elements of the ArrayList
+    // function to print all elements
     public static void printList(ArrayList<Integer> list) {
         for (int i = 0; i <= list.size(); i++) {
             System.out.print(list.get(i) + " ");
@@ -28,20 +28,16 @@ public class MyArrayList {
     public static void main(String[] args) {
         MyArrayList list = new MyArrayList();
 
-        // Adding elements to the ArrayList
         list.add(args[0]);
         list.add(args[1]);
 
-        // Attempting to access an element beyond the list size
         try {
-            System.out.println("Element at the last index: " + list.get(2)); // This will cause an exception
+            System.out.println("Element at the last index: " + list.get(2));
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Caught an IndexOutOfBoundsException!");
         }
         printList(list);
-
-        // Correctly printing elements of the ArrayList
-        myArrayList.printList();
+        
     }
 }
 
