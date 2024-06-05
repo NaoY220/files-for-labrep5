c35f27a40a376a8b17e561fb9f99f77448e11105
NaoY220 <129801469+NaoY220@users.noreply.github.com>
2024-06-04 21:25:39 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
+javac -cp .:path/to/junit-jupiter-api-5.7.1.jar:path/to/junit-jupiter-engine-5.7.1.jar MyArrayList.java MyArrayListTest.java
+java -cp .:path/to/junit-jupiter-api-5.7.1.jar:path/to/junit-jupiter-engine-5.7.1.jar org.junit.platform.console.ConsoleLauncher --select-class MyArrayListTest
+
+if [[ $? -eq 0 ]]; then
+    echo "yay"
+else
+    echo "ummm"
+
 JAVA_FILE="MyArrayList.java"
 CLASS_NAME="MyArrayList"
 
